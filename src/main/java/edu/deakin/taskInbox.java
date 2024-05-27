ad1375a20ea3cc3cb99d915e8c62411247226e63
mehmehm <s221514619@deakin.edu.au>
2024-05-27 19:33:23 +1000

    updated code

--- src/main/java/edu/deakin/taskInbox.java
+++ src/main/java/edu/deakin/taskInbox.java
@@ -1,10 +1,24 @@
 package edu.deakin;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class taskInbox {
 	
 	public List<task> getTaskList(){
-		return null;
+		
+		List<task> taskList = new ArrayList<>();
+		
+		task task1 = new task("P1", "intro to coding", "pass", "completed", "-");
+		task task2 = new task("P2", "hello world", "pass", "completed", "-");
+		task task3 = new task("C1", "if-else statement", "credit", "discuss", "2 weeks");
+		task task4 = new task("D1", "algorithms", "distinction", "resubmit", "3 weeks");
+		
+		taskList.add(task1);
+		taskList.add(task2);
+		taskList.add(task3);
+		taskList.add(task4);
+		
+		return taskList;
 	}
  }
